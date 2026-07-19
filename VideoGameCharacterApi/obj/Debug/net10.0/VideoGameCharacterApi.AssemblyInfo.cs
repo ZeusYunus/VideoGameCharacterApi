451ba0101b009fba4d274b3b7344dcbdfa1db1e4
Nizaam Yunus <nizaamcoleyunus18@gmail.com>
2026-07-19 17:07:23 +0200

    Updated the controller and test if the project is still working

--- VideoGameCharacterApi/obj/Debug/net10.0/VideoGameCharacterApi.AssemblyInfo.cs
+++ VideoGameCharacterApi/obj/Debug/net10.0/VideoGameCharacterApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VideoGameCharacterApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38fa755fe8e0a56ae728e12262fe8d8ac4ba8ec7")]
 [assembly: System.Reflection.AssemblyProductAttribute("VideoGameCharacterApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VideoGameCharacterApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
